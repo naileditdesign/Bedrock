9a68a6d887120a83a999256ffbdfe0f7e51509aa
naileditdesign <adam@naileditdesign.com>
2013-08-07 12:25:48 +0100

    Initial Upload of Files for 1.0.1

---
+++ js/extend.js
@@ -0,0 +1,14 @@
+// Add all your lovely no conflict Jquery extensions, you mutha!
+
+
+!function ($) {
+
+  // be good and annotate your extensions!
+ 
+ // Carousel
+ 
+ //Just like that!
+ 
+
+
+}(window.jQuery)
